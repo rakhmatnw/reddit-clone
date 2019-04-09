e6351d62feba18a6dd23fd3648c8c30002b9a4f9
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 12:03:11 +0700

    initial setup of react helmet & added cdn for boostrap styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Helmet} from 'react-helmet';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Using React Helmet to edit html head tag */}
+        <Helmet>
+          <title>Reddit Clone</title>
+          {/* cdn for react-bootstrap styling */}
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous"
+          />
+        </Helmet>
+
       </div>
     );
   }
