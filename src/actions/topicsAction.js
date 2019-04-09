088dffa67039b976ed5f2cf17d08ec71aa102c14
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 15:46:41 +0700

    added addTopic action cretor

--- src/actions/topicsAction.js
+++ src/actions/topicsAction.js
@@ -3,3 +3,17 @@ export const getTopics = () => {
     type: 'GET_TOPICS'
   }
 }
+
+export const addTopic = (content, currentTopics) => {
+  const payload = {
+    id: currentTopics.length + 1,
+    content: content,
+    upvote: 0,
+    downvote: 0
+  }
+
+  return {
+    type: 'ADD_TOPICS',
+    payload: payload
+  }
+}
