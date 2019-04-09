221899bc18d9699e8176491e640b4d8f461658db
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 15:22:29 +0700

    connect the component with redux store to display it to TopicItem component

--- src/components/TopicsContainer.js
+++ src/components/TopicsContainer.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import {ListGroup} from 'react-bootstrap';
 import TopicItem from './TopicItem';
+import {connect} from 'react-redux';
+import {getTopics} from './../actions/topicsAction';
 
 class TopicsContainer extends Component {
 
+  componentDidMount(){
+    this.props.getTopics();
+  }
+
   render(){
+    const TopicList = this.props.topics.map((topic,i) => {
+      return(
+        <TopicItem 
+          key={topic.id}
+          id={topic.id}
+          index={i+1}
+          content={topic.content}
+          upvote={topic.upvote}
+          downvote={topic.downvote}/>
+      )
+    })
+
     return(
       <ListGroup>
-        <TopicItem />
+        {TopicList}
       </ListGroup>
     )
   }
 }
 
-export default TopicsContainer;
+const mapStateToProps = store => {
+  return {
+    topics: store.topics
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getTopics: () => {dispatch(getTopics())}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopicsContainer);
