874ba952795dedf7dc7a6e9bc8788131f49bbf52
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 15:21:31 +0700

    added conditional retun for GET_TOPICS action and added temporary hardcoded data

--- src/reducers/topicsReducer.js
+++ src/reducers/topicsReducer.js
@@ -1,7 +1,26 @@
 const initState = {
-  topics: []
+  topics: [
+    {
+      id: 0,
+      content: 'this is a topic that will get alot of upVote',
+      upvote: 142,
+      downvote: 5
+    },
+    {
+      id: 1,
+      content: 'this is a second topic with downvote',
+      upvote: 31,
+      downvote: 544
+    }
+  ]
 }
 
 export const topicsReducer = (state = initState, action) => {
+  if(action.type === 'GET_TOPICS'){
+    return {
+      topics: state.topics
+    }  
+  }
+
   return state
 }
