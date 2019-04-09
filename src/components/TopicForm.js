8a70a81903e6c41bac1c9ae6622f2813eade484a
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 13:55:46 +0700

    added modal to be shown when user submit a post but the character length is more than 255

--- src/components/TopicForm.js
+++ src/components/TopicForm.js
@@ -4,12 +4,14 @@ import {
   Form, 
   Button, 
   Row,
-  Col
+  Col,
+  Modal
 } from 'react-bootstrap';
 
 class TopicForm extends Component {
   state = {
     content: '',
+    modalShown: false
   }
 
   handleChange = e => {
@@ -18,33 +20,72 @@ class TopicForm extends Component {
     })
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    if(this.state.content.length > 255){
+      this.setState({
+        modalShown: true
+      })
+    }
+  }
+
+  handleModalToggle = () => {
+    this.setState({
+      modalShown: false
+    })
+  }
+
   render(){
+    // destructure 'content' from state
     let {content} = this.state;
+    
     return(
-      // I used Alert to wrap the form and give it background color of 'info' from Bootstrap color pallete styling
-      <Alert variant={content.length <= 255 ? "info" : "danger"}>
-        <Form>
-          <Form.Control 
-            name="content"
-            as="textarea" 
-            rows="3" 
-            style={{marginBottom: '10px'}}
-            onChange={this.handleChange}
-            />
-          <Row>
-            <Col md={3}>
-              <span>{this.state.content.length} / 255</span>
-            </Col>
-            <Col md={{ span: 3, offset: 6}}>
-              <Button 
-                variant="primary"
-                block>
-                post
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </Alert>
+      <div>
+        {/* I used Alert to wrap the form and give it background color of 'info' from Bootstrap color pallete styling and will turn to 'danger' if content character length is more than 255  */}
+        <Alert variant={content.length <= 255 ? "info" : "danger"}>
+          <Form>
+            {/* Form.Control is a component from React-Bootstrap */}
+            <Form.Control 
+              name="content"
+              as="textarea" 
+              rows="3" 
+              style={{marginBottom: '10px'}}
+              onChange={this.handleChange}
+              />
+            {/* I used react-bootstrap layout grid with Row & Col */}
+            <Row>
+              <Col md={3}>
+                <span>{this.state.content.length} / 255</span>
+              </Col>
+              <Col md={{ span: 3, offset: 6}}>
+                <Button 
+                variant="primary" 
+                block
+                onClick={this.handleSubmit}> 
+                  Post 
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Alert>
+
+        {/* Modal will shown when user submit a post but the character length is more than 255 character */}
+        <Modal 
+          show={this.state.modalShown} 
+          onHide={this.handleModalToggle}
+          >
+          <Modal.Body>
+            The number of Topic characters cannot be more than 255 character!
+          </Modal.Body>
+          <Modal.Footer>
+            <Button 
+              variant="secondary"
+              onClick={this.handleModalToggle}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     )
   }
 }
