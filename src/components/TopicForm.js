1a7d591ba6e5f198e48cfd2694b1c700b22399c2
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-09 12:41:30 +0700

    added UI changes to tell user when the character of topic content is more than 255

--- src/components/TopicForm.js
+++ src/components/TopicForm.js
@@ -8,23 +8,39 @@ import {
 } from 'react-bootstrap';
 
 class TopicForm extends Component {
+  state = {
+    content: '',
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render(){
+    let {content} = this.state;
     return(
       // I used Alert to wrap the form and give it background color of 'info' from Bootstrap color pallete styling
-      <Alert variant="info">
+      <Alert variant={content.length <= 255 ? "info" : "danger"}>
         <Form>
           <Form.Control 
+            name="content"
             as="textarea" 
             rows="3" 
-            style={{
-              marginBottom: '10px'
-            }}/>
+            style={{marginBottom: '10px'}}
+            onChange={this.handleChange}
+            />
           <Row>
-            <Col>
-              <span>0/255</span>
+            <Col md={3}>
+              <span>{this.state.content.length} / 255</span>
             </Col>
-            <Col style={{textAlign: 'right'}}>
-              <Button variant="primary">post</Button>
+            <Col md={{ span: 3, offset: 6}}>
+              <Button 
+                variant="primary"
+                block>
+                post
+              </Button>
             </Col>
           </Row>
         </Form>
