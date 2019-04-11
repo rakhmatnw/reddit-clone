e8d2469f65490634899c0f6f422b91df0188a3a5
rakhmatnw <rakhmatnashrul@gmail.com>
2019-04-11 11:53:39 +0700

    create-react-app on Heroku

---
+++ build/precache-manifest.ff43c0c9782dc8074b5333f7cbce5995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4639d49c2f57d0caf056",
+    "url": "/static/js/main.dd73fcdb.chunk.js"
+  },
+  {
+    "revision": "04144a692497288246d6",
+    "url": "/static/js/2.da694696.chunk.js"
+  },
+  {
+    "revision": "4639d49c2f57d0caf056",
+    "url": "/static/css/main.700b60f9.chunk.css"
+  },
+  {
+    "revision": "3f5a2a6c5beda8efb43310aa4869eee6",
+    "url": "/index.html"
+  }
+];
